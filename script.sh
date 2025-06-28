915b4ea8accd17675db900cf91b19dbae2be7671
danis-devaccount <dev@zojl.dev>
2025-06-28 02:21:01 +0200

    Get image back

--- script.sh
+++ script.sh
@@ -66,6 +66,8 @@ if [ ! -f "$PANEL_DIR/docker-compose.yml" ]; then
     exit 1
 fi
 
+sed -i '/^[[:space:]]*3x-ui:$/a\    image: ghcr.io/mhsanaei/3x-ui:latest' файл.yaml
+
 generate_random_string() {
     tr -dc A-Za-z0-9 </dev/urandom | head -c 16
 }
